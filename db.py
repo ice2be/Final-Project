6a99db72c315d761eba62995cde03990f050f922
Ice <nogueiran55@gmail.com>
2024-12-10 19:27:29 -0500

    Update db.py

--- db.py
+++ db.py
@@ -2,10 +2,10 @@ import mysql.connector
 
 def get_db_connection():
     db = mysql.connector.connect(
-        host="ice2be.com",
-        port="40355",
-        user="root",
-        password="oddball55",
-        database="florida_international_university"
+        host="####",
+        port="####",
+        user="####",
+        password="####",
+        database="####"
     )
     return db
